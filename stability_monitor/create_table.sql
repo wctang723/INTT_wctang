ce0469fe120f5715bf1d32764a21ff7bfc0d0b64
wctang@mac <leventseleve.amos@gmail.com>
2024-04-01 23:28:41 +0800

    Create table sql

--- stability_monitor/create_table.sql
+++ stability_monitor/create_table.sql
@@ -1,8 +1,13 @@
 CREATE TABLE (
-	id BIGSERIAL,
-	run_number INT,
+	run_id BIGSERIAL PRIMARY KEY,
+	run_number INT NOT NULL,
 	event_number INT,
 	hit_number INT,
-	hit_ratio DOUBLE,
-
+	hit_rate REAL,
+	hot_channel_number REAL,
+	dead_channel_number REAL,
+	cold_channel_number REAL,
+	update_time DATE NOT NULL,
+	update_version INT NOT NULL,
+	comment VARCHAR(300)
 );
